55ffab320c09f8a265b30e4d224b2181a27e07f6
krishna-verma2006 <krishnaverma2006.kv@gmail.com>
2026-09-08 19:56:15 +0530

    updated server

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import app from './src/app.js'
+import { configDotenv } from 'dotenv';
+
+configDotenv();
 
 app.listen(process.env.PORT,()=>{
     console.log("server is running on port",process.env.PORT);
